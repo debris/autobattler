42217ee1e1d1a3b10c9859a6f8a3bfd34ed64d9e
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-02-10 21:32:13 +0300

    sub defence using engine compare

--- Engine/Compare.gd
+++ Engine/Compare.gd
@@ -2,12 +2,17 @@ extends Object
 class_name Compare
 
 static func highest_def(unit_a: BattleUnit, unit_b: BattleUnit):
-	if unit_a.def > unit_b.def:
+	if unit_a.def >= unit_b.def:
+		return unit_a
+	return unit_b
+
+static func lowest_def(unit_a: BattleUnit, unit_b: BattleUnit):
+	if unit_a.def <= unit_b.def:
 		return unit_a
 	return unit_b
 
 static func highest_dmg(unit_a: BattleUnit, unit_b: BattleUnit):
-	if unit_a.dmg > unit_b.dmg:
+	if unit_a.dmg >= unit_b.dmg:
 		return unit_a
 	return unit_b
  
