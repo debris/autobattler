3fac980642c17f48c9ae6f14cca9c1640bcf1ee1
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-02-10 21:36:22 +0300

    JackCross uses Engine Compare

--- Engine/Skills/DoubleCross.gd
+++ Engine/Skills/DoubleCross.gd
@@ -7,11 +7,5 @@ func _init():
 
 func _execute(query: BattleQuery) -> Array[Log]:
 	var unit = query.get_this_unit()
-	var enemy_team = query.get_enemy_team()
-	var enemy_unit_to_swap = enemy_team.members[0]
-	for i in enemy_team.members.size() - 1:
-		var enemy_unit = enemy_team.members[i + 1]
-		if enemy_unit_to_swap == null || (enemy_unit != null && (enemy_unit.def > enemy_unit_to_swap.def)):
-			enemy_unit_to_swap = enemy_unit
-	
+	var enemy_unit_to_swap = query.get_enemy_team().iterator().compare(Compare.highest_def)
 	return [LogSkillUsed.new(unit, self), LogSwapPlaces.new(unit, enemy_unit_to_swap)]
