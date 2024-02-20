dc46a9c5157ab46999fb356981cadd3c9e691f17
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-02-20 16:25:38 +0530

    updated unit details and help with the new unit control

--- UI/Help.gd
+++ UI/Help.gd
@@ -1,5 +1,23 @@
 extends Control
 
+@onready var unit_control = $ScrollContainer/GridContainer/Unit/UnitControl
+
+func _ready():
+	var help_unit = UnitCaptainBlaze.new()
+	var schedules: Array[Schedule] = [
+		Schedule.new(),
+		Schedule.new(),
+		Schedule.new()
+	]
+	schedules[0].kind = Schedule.Kind.SKILL
+	schedules[0].data = [true, false, false] as Array[bool]
+	schedules[1].kind = Schedule.Kind.DEF
+	schedules[1].data = [false, true, false, false, true] as Array[bool]
+	schedules[2].kind = Schedule.Kind.DMG
+	schedules[2].data = [false, false, false, false, false, true, true, true] as Array[bool]
+	var owned_unit = OwnedUnit.new(help_unit, schedules)
+	unit_control.unit = owned_unit
+
 func _gui_input(event):
 	var mouse_position = get_global_mouse_position()
 	var rect = get_global_rect()
