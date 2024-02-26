7bc54b0c2617b338437bfe0b57c373a3153018c7
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-02-26 20:33:07 +0100

    basic validation of the selected name

--- UI/SelectName.gd
+++ UI/SelectName.gd
@@ -4,6 +4,7 @@ signal action_selected(SelectNameAction)
 
 @onready var text_edit = $CenterContainer/GridContainer/TextEdit
 @onready var global_overlay = $GlobalOverlay
+@onready var name_already_exists = $NameExistsLabel
 
 func _ready():
 	global_overlay.exit_button.visible = true
@@ -20,7 +21,15 @@ func _on_proceed_button_pressed():
 func _on_text_edit_text_changed():
 	if text_edit.text.contains("\n"):
 		var selected_name = text_edit.text.replace("\n", "")
+		text_edit.text = selected_name
+		text_edit.set_caret_column(selected_name.length())
 		proceed(selected_name)
+	
+	if Save.exists(text_edit.text):
+		name_already_exists.visible = true
+		return
+
+	name_already_exists.visible = false
 
 func proceed(text):
 	if Save.exists(text):
