c31f81a5927b8fb7c5326d00e8a83bdf3b615bf6
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-02-27 16:29:56 +0100

    parallax background effect

---
+++ UI/Background.gd
@@ -0,0 +1,10 @@
+@tool
+extends Sprite2D
+
+static var shared_region_position = 255.0
+
+func _process(_delta):
+	var global_time = float(Time.get_ticks_msec()) / 1000.0;
+	const CONCRETE_PACE: float = 25.0
+	shared_region_position = -global_time * CONCRETE_PACE
+	region_rect.position.y = shared_region_position
